c1bb2cb6fcebc9b5923db03e803c8e0fa3a507f2
Hamza Khatri <116482376+code-with-Hamza667@users.noreply.github.com>
2023-10-13 12:41:39 +0500

    algo added

---
+++ index.js
@@ -0,0 +1,32 @@
+//Consensus Algorithm
+
+const crypto = require('crypto');
+let difficulty =4;
+let target = '0'.repeat(difficulty); //target is leading 4 zeroes. so we represent this by using this line
+let nonce;
+let nonceMax = Math.pow(2,30);       //max range of nonce that can be produced
+
+function proofOfWork(data, difficulty){
+    for(nonce = 0; nonce<nonceMax; nonce++){
+        let message = data + nonce.toString();
+        let hash = crypto.createHash('sha256').update(message).digest('hex');
+        if(hash.substring(0,3)===target){
+            console.log(`Hash Found ${hash}`);
+        }
+        else{
+            console.log('hash not found');
+        }
+
+    }
+    return null;
+}
+
+let data = "Mr.Abdul Rafay sent 1 BTC to Mr. Hammad";
+let timeStampStart = Date.now();
+let pow = proofOfWork(data, difficulty);
+let timeStampEnd = Date.now();
+if(pow != null){
+    let time = (timeStampEnd - timeStampStart)/1000;
+    console.log(`Time for Hash calculation: ${time}`);
+
+}
